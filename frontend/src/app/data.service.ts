440e930471694dd93e25757c31cace30370337b8
Cam Roe <camroe@gmail.com>
2018-05-30 11:18:51 -0600

    Change Angular data service to use post for /api/logEntry call to produce mock pdf.

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -1,13 +1,24 @@
 import {Injectable} from '@angular/core';
 import {LogEntry} from "./log-entry";
-import {HttpClient, HttpResponse} from "@angular/common/http";
+import {HttpClient, HttpHeaders,HttpErrorResponse} from "@angular/common/http";
+import {Observable,throwError} from "rxjs/index";
+import {catchError} from "rxjs/internal/operators";
+
+const postHttpOptions = {
+    headers: new HttpHeaders({
+        'Content-type': 'application/json'
+    })
+};
+
 
 @Injectable({
     providedIn: 'root'
 })
 export class DataService {
 
-    private logEntriesURL = 'api/logEntries'
+    private logEntriesURL = 'api/logEntries';
+    private logEntryURL = 'api/logEntry';
+
 
     constructor(private httpClient: HttpClient) {
     }
@@ -32,9 +43,33 @@ export class DataService {
         return this.httpClient.get<LogEntry[]>(this.logEntriesURL);
     }
 
-
     private handleError(error: any): Promise<any> {
         console.error('Error', error); // for demo purposes only
         return Promise.reject(error.message || error);
     }
+
+    public postLogEntry(logEntry: LogEntry) {
+        return this.httpClient.post(this.logEntryURL,logEntry,postHttpOptions)
+            .pipe(
+                // catchError(this.handleErrorVerbose('postLogEntry',logEntry))
+        catchError(this.handleErrorVerbose)
+            );
+    }
+    private handleErrorVerbose(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.error('An error occurred:', error.error.message);
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            console.error(
+                `Backend returned code ${error.status}, ` +
+                `body was: ${error.error}`);
+        }
+        // return an observable with a user-facing error message
+        return throwError(
+            'Something bad happened; please try again later.');
+    };
+
 }
+
